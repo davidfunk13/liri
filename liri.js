bd160b39ca3fb3d27ba08446636e0cdc73373d54
Dave <davefunk135@gmail.com>
2018-02-02 15:27:44 -0800

    requires

--- liri.js
+++ liri.js
@@ -1,7 +1,13 @@
+//Require Dotenv
 require("dotenv").config();
 
+//Require Keys
 var keys = require('./keys.js');
-
+//Reference to Keys
 var spotify = new Spotify(keys.spotify);
 var twitterClient = new Twitter(keys.twitter);
 
+//Require Twitter
+var twitter = require('twitter');
+//Require Spotify
+var requireSpotify = require('node-spotify-api');
