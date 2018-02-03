f56addbf83ed26feb16da1c60ffc837d3e11a547
Dave <davefunk135@gmail.com>
2018-02-02 16:11:04 -0800

    switch cases added for functions

--- liri.js
+++ liri.js
@@ -15,4 +15,17 @@ var requireSpotify = require('node-spotify-api');
 //Capture Index 2 as Argument
 var theArg = process.argv[2];
 
-
+switch (theArg) {
+    case 'my-tweets':
+        callTwitter();
+        break;
+    case 'spotify-this-song':
+        callSpotify();
+        break;
+    case 'movie-this':
+        callOmdb();
+        break;
+    case 'do-what-it-says':
+        callTheFile();
+        break;
+}
