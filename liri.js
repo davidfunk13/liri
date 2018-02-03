927ed7206046095ad8fb046b07e0b4becec06325
Dave <davefunk135@gmail.com>
2018-02-02 16:36:45 -0800

    response from twitter successfull, need to manipulate object now.

--- liri.js
+++ liri.js
@@ -1,20 +1,32 @@
 //Require Dotenv
 require("dotenv").config();
-
+//Require Twitter
+var twitter = require('twitter');
 //Require Keys
 var keys = require('./keys.js');
 //Reference to Keys
-var spotify = new Spotify(keys.spotify);
-var twitterClient = new Twitter(keys.twitter);
+// var spotify = new Spotify(keys.spotify);
+var twitterClient = new twitter(keys.twitter);
 
-//Require Twitter
-var twitter = require('twitter');
 //Require Spotify
 var requireSpotify = require('node-spotify-api');
 
 //Capture Index 2 as Argument
 var theArg = process.argv[2];
 
+//Twitter Function
+function callTwitter() {
+    twitterClient.get('statuses/user_timeline', {screen_name: 'davefunk135'}, function (error, tweets, response) {
+        if (error) {
+            console.log('there was an error getting tweets');
+            console.log(error);
+        }
+        console.log(tweets);
+        console.log(response);
+    } )
+};
+
+//Switch Case Scenarios for theArg
 switch (theArg) {
     case 'my-tweets':
         callTwitter();
