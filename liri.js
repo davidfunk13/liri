31e20b645f0d9d9566b71a5f2fec6527b50f89a2
Dave <davefunk135@gmail.com>
2018-02-03 19:21:57 -0800

    added ability to read file random.txt and run those commands. going to add ability to write to the file, and if there is time going to work with integrating inquire to prune results and add more functionality.

--- liri.js
+++ liri.js
@@ -32,44 +32,108 @@ function callTwitter() {
             console.log('there was an error getting tweets');
             console.log(error);
         }
+        console.log('Heres user davefunk135s tweets:')
         for (var i = 0; i < tweets.length; i++) {
-            console.log('Heres user davefunk135s tweets:')
-            console.log(tweets[i].text)
+            console.log('Tweet ' + i + ": " + tweets[i].text)
         }
         // console.log(response);
     })
 };
 
 //Spotify Functionality
-function callSpotify() {
+function callSpotify(fileParam = 0) {
+    console.log(fileParam)
     var songName = process.argv[3]
     spotify.search({
-            type: 'track',
-            query: songName
-        }).then(function (response) {
-            // console.log(response.tracks.items)
-            // console.log({'Artists: ' : response.tracks.items[1].album.artists[0].name}, {'Preview Url: ': response.tracks.items[1].preview_url}, {'Song Name: ': response.tracks.items[1].name});
-            //artistsloop
-            for (var i = 0; i < 5; i++) {
-                console.log('yoyo' + i)
-                console.log(
-                    {
+        type: 'track',
+        query: songName || fileParam
+    }).then(function (response) {
+        // console.log(response.tracks.items)
+        // console.log({'Artists: ' : response.tracks.items[1].album.artists[0].name}, {'Preview Url: ': response.tracks.items[1].preview_url}, {'Song Name: ': response.tracks.items[1].name});
+        //artistsloop
+        for (var i = 0; i < 5; i++) {
+            console.log('Result ' + i + ':')
+            console.log(
+                {
                     'Artist Name: ': response.tracks.items[i].artists[0].name
-                }
-                , {
+                }, 
+                {
                     'Preview URL: ': response.tracks.items[i].preview_url
-                }, {
+                }, 
+                {
                     'Song Name: ': response.tracks.items[i].name
                 },
-            {
-                'album: ' : response.tracks.items[i].album.name
-            }
+                {
+                    'album: ': response.tracks.items[i].album.name
+                }
             );
-            };
-        })
+        };
+    })
         .catch(function (err) {
             console.log(err);
         });
+}
+
+//function for omdb
+function callOmdb() {
+    var movieName = process.argv[3];
+    var omdbQueryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    request(omdbQueryUrl, function(error, response, body) {
+        if (!error && response.statusCode === 200){
+            // console.log(JSON.parse(body));
+            console.log(
+                {
+                    'Movie Name: ': JSON.parse(body).Title
+                }, 
+                {
+                    'Release: ': JSON.parse(body).Year
+                }, 
+                {
+                    'IMDB Rating: ': JSON.parse(body).Ratings[0].Value
+                },
+                {
+                    'Rotten Tomatoes: ': JSON.parse(body).Ratings[1].Value
+                },
+                {
+                    'Country of Production: ': JSON.parse(body).Country
+                },
+                {
+                    'Language: ': JSON.parse(body).Language
+                },
+                {
+                    'Plot: ': JSON.parse(body).Plot
+                },
+                {
+                    'Featured Actors: ': JSON.parse(body).Actors
+                }
+            );
+        }
+    })
+
+console.log(omdbQueryUrl);
+
+}
+
+// functionality for fs read
+
+function callTheFile() {
+    fs.readFile('random.txt', 'utf8', function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data);
+        var contentArray = data.split(",");
+
+        console.log(contentArray[0]);
+        console.log(contentArray[1]);
+        //Capture Index 2 as Argument
+        var fileArg = contentArray[0];
+        var fileParam = contentArray[1];
+        // switch(fileArg) {
+            // case 'spotify-this-song':
+                callSpotify(fileParam);
+        // }
+    })
 }
 
 //Switch Case Scenarios for theArg
